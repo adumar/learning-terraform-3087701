ba9316fc0dac2e53d382dc079becc075bc2bf6c3
adumar <adumar@gmail.com>
2023-07-11 09:27:00 -0500

    fix nat gateway?

--- main.tf
+++ main.tf
@@ -23,7 +23,9 @@ module "vpc" {
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
+  single_nat_gateway     = false
+  enable_nat_gateway     = true
+  one_nat_gateway_per_az = true
 
   tags = {
     Terraform = "true"
