b54b01b343b856bd3ad8223bc3d7b84a6f419cc7
adumar <adumar@gmail.com>
2023-07-24 10:16:11 -0500

    anhtoher typo

--- variables.tf
+++ variables.tf
@@ -14,8 +14,8 @@ variable "ami_filter" {
   default = {
     name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
     owner = "979382823631"
+  }
 }
-
 
 variable "environment" {
   description = "Development Environment"
